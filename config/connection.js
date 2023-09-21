9a99e16f183929ea823c584f3c37060223d03737
Aleenabrink93 <aleenabrink@gmail.com>
2023-09-21 16:44:38 -0600

    fixed bugs

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,8 @@
-const mongoose = require('mongoose');
+const { connect, connection } = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialmedia', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialnetworkDB';
 
-module.exports = mongoose.connection;
+connect(connectionString);
+
+module.exports = connection;
